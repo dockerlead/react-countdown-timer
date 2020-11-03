0a055b5bf244b48f57bdf24edece8a2ea5cac69d
dockerlead <dockerlead@gmail.com>
2020-11-03 01:13:37 -0600

    :construction:Adds timer logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear()
+    const difference = +new Date(`12/25/${year}`) - +new Date()
+    let timeLeft = {}
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      }
+    }
+    return timeLeft;
+  }
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
+  const [year] = useState(new Date().getFullYear())
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft())
+    }, 1000)
+    return () => clearTimeout(timer)
+  })
+  const timerComponents = []
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return
+    }
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    )
+  })
+
   return (
-    <></>
+  <div>
+    <h1>Christmas {year} Countdown</h1>
+    {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+  </div>  
   );
 }
 
